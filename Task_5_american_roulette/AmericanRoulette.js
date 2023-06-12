23fb3142019f576ac808442af905dcc8a833c988
Lenoczka <nerubenko.elena@bk.ru>
2023-06-13 00:42:18 +0300

    refactoring code: adding functions adding new bet types

--- Task_5_american_roulette/AmericanRoulette.js
+++ Task_5_american_roulette/AmericanRoulette.js
@@ -1,142 +1,195 @@
 var numbers = [
-    { value: 0, color: -1, colorUI: "green", type: "zero" },
-    { value: 1, color: 0, colorUI: "red", type: "odd" },
-    { value: 2, color: 1, colorUI: "black", type: "even" },
-    { value: 3, color: 0, colorUI: "red", type: "odd" },
-    { value: 4, color: 1, colorUI: "black", type: "even" },
-    { value: 5, color: 0, colorUI: "red", type: "odd" },
-    { value: 6, color: 1, colorUI: "black", type: "even" },
-    { value: 7, color: 0, colorUI: "red", type: "odd" },
-    { value: 8, color: 1, colorUI: "black", type: "even" },
-    { value: 9, color: 0, colorUI: "red", type: "odd" },
-    { value: 10, color: 1, colorUI: "black", type: "even" },
-    { value: 11, color: 1, colorUI: "black", type: "odd" },
-    { value: 12, color: 0, colorUI: "red", type: "even" },
-    { value: 13, color: 1, colorUI: "black", type: "odd" },
-    { value: 14, color: 0, colorUI: "red", type: "even" },
-    { value: 15, color: 1, colorUI: "black", type: "odd" },
-    { value: 16, color: 0, colorUI: "red", type: "even" },
-    { value: 17, color: 1, colorUI: "black", type: "odd" },
-    { value: 18, color: 0, colorUI: "red", type: "even" },
-    { value: 19, color: 0, colorUI: "red", type: "odd" },
-    { value: 20, color: 1, colorUI: "black", type: "even" },
-    { value: 21, color: 0, colorUI: "red", type: "odd" },
-    { value: 22, color: 1, colorUI: "black", type: "even" },
-    { value: 23, color: 0, colorUI: "red", type: "odd" },
-    { value: 24, color: 1, colorUI: "black", type: "even" },
-    { value: 25, color: 0, colorUI: "red", type: "odd" },
-    { value: 26, color: 1, colorUI: "black", type: "even" },
-    { value: 27, color: 0, colorUI: "red", type: "odd" },
-    { value: 28, color: 1, colorUI: "black", type: "even" },
-    { value: 29, color: 1, colorUI: "black", type: "odd" },
-    { value: 30, color: 0, colorUI: "red", type: "even" },
-    { value: 31, color: 1, colorUI: "black", type: "odd" },
-    { value: 32, color: 0, colorUI: "red", type: "even" },
-    { value: 33, color: 1, colorUI: "black", type: "odd" },
-    { value: 34, color: 0, colorUI: "red", type: "even" },
-    { value: 35, color: 1, colorUI: "black", type: "odd" },
-    { value: 36, color: 0, colorUI: "red", type: "even" }
+    { value: 0, colorUI: "green", type: "zero" },
+    { value: 1, colorUI: "red", type: "odd" },
+    { value: 2, colorUI: "black", type: "even" },
+    { value: 3, colorUI: "red", type: "odd" },
+    { value: 4, colorUI: "black", type: "even" },
+    { value: 5, colorUI: "red", type: "odd" },
+    { value: 6, colorUI: "black", type: "even" },
+    { value: 7, colorUI: "red", type: "odd" },
+    { value: 8, colorUI: "black", type: "even" },
+    { value: 9, colorUI: "red", type: "odd" },
+    { value: 10, colorUI: "black", type: "even" },
+    { value: 11, colorUI: "black", type: "odd" },
+    { value: 12, colorUI: "red", type: "even" },
+    { value: 13, colorUI: "black", type: "odd" },
+    { value: 14, colorUI: "red", type: "even" },
+    { value: 15, colorUI: "black", type: "odd" },
+    { value: 16, colorUI: "red", type: "even" },
+    { value: 17, colorUI: "black", type: "odd" },
+    { value: 18, colorUI: "red", type: "even" },
+    { value: 19, colorUI: "red", type: "odd" },
+    { value: 20, colorUI: "black", type: "even" },
+    { value: 21, colorUI: "red", type: "odd" },
+    { value: 22, colorUI: "black", type: "even" },
+    { value: 23, colorUI: "red", type: "odd" },
+    { value: 24, colorUI: "red", type: "odd" },
+    { value: 26, colorUI: "black", type: "even" },
+    { value: 27, colorUI: "red", type: "odd" },
+    { value: 28, colorUI: "black", type: "even" },
+    { value: 29, colorUI: "black", type: "odd" },
+    { value: 30, colorUI: "red", type: "even" },
+    { value: 31, colorUI: "black", type: "odd" },
+    { value: 32, colorUI: "red", type: "even" },
+    { value: 33, colorUI: "black", type: "odd" },
+    { value: 34, colorUI: "red", type: "even" },
+    { value: 35, colorUI: "black", type: "odd" },
+    { value: 36, colorUI: "red", type: "even" }
 ];
 
-let initialBalance = prompt("initial balance $:");
-while (initialBalance > 0) {
-    var moneyForBet = prompt("make a bet: amount $:");
-    var betValue = prompt("enter the straight or enter skip for choosing the color: ");
-    var betColor = prompt("enter the color or enter skip for choosing the even or odd: ");
-    var betEvenOdd = prompt("enter the even or odd skip if u chose the straight/color: ");
-    var index = Math.floor(Math.random()*37);
-    var resultRoulette = numbers[index];
-    if (resultRoulette.value == betValue) {
-       initialBalance=initialBalance + moneyForBet*35;
-       alert("0 Win " + resultRoulette.value + " " + resultRoulette.colorUI + " " + ", Your " + betValue + " win, " + "balance left: " + initialBalance);
-    } else if (resultRoulette.value != betValue) {
-        initialBalance=initialBalance-moneyForBet;
-        alert("1 Win " + resultRoulette.value + " " + resultRoulette.colorUI + " " + ", Your " + betValue +  " " + "loss, " + "balance left: " + initialBalance);
-    } else if (resultRoulette.colorUI == betColor) {
-       initialBalance=initialBalance + moneyForBet*2;
-       alert("2 Win " + resultRoulette.value + " " + resultRoulette.colorUI +  " "  +  ", Your " + betColor +  " win, " + "balance left: " + initialBalance);
-    } else   if (resultRoulette.colorUI != betColor){
-       initialBalance=initialBalance-moneyForBet;
-       alert("3 Win " + resultRoulette.value + " " + resultRoulette.colorUI + ", Your " + betColor + " loss, " + "balance left: " + initialBalance);
-    } else if (resultRoulette.type ==betEvenOdd) {
-        initialBalance=initialBalance + moneyForBet*2;
-        alert("4 Win " + resultRoulette.value + "" + resultRoulette.type +  ", Your " + betEvenOdd  + " win, " + "balance left: " + initialBalance);
-    } else  if (resultRoulette.type !=betEvenOdd) {
-        initialBalance=initialBalance - moneyForBet;
-        alert("5 Win " + resultRoulette.value + "" + resultRoulette.type   + ", Your " +  betEvenOdd + " loss, " + "balance left: " + initialBalance);
-    }
-}
 
 //Run the game
 runGame()
 
 function runGame() {
-    let currentBalance = prompt("How much money do you have?");
+    var currentBalance = prompt("How much money do you have $"); /let *позволяет объявить локальную переменную с областью видимости*/
 
     while (currentBalance > 0) {
         var bet = makeBet(currentBalance)
 
-        //say bets maked...
-
         var rouletteResult = rollRoulette()
 
-        var isBetWon = checkBet(bet, rouletteResult)
+        var isBetWon = isBetWon(bet, rouletteResult)
 
-        //adjusting balance
         if (isBetWon) {
-
+            //adjusting balance
+            switch (bet.type) { //depends on bet's type
+                case betType.STRAIGHT:
+                    currentBalance = currentBalance + bet.amount*35;
+                    break;
+                case betType.COLOR:
+                    currentBalance = currentBalance + bet.amount*2;
+                    break;
+                case betType.EVENODD:
+                    currentBalance == currentBalance + bet.amount*2;
+                    break;
+                case betType.DOZEN:
+                    currentBalance = currentBalance + bet.amount*2;
+                    break;
+                case betType.COLUMN:
+                    currentBalance == currentBalance + bet.amount*3;
+                    break;
+                case betType.ROW:
+                    currentBalance == currentBalance + bet.amount*11;
+                    break;
+            }
         } else {
-
+            currentBalance = currentBalance - bet.amount;
         }
     }
 }
 
-//enum!!!!
-var betTypes = [
-    "strait",
-    "split",
-    "street",
-    "corner",
-    //etc
-]
+//make enum
+const betType = {
+    STRAIGHT: "straight" ,
+    COLOR: "color",
+    EVENODD: "EvenOdd",
+    DOZEN: "dozen",
+    COLUMN: "columns",
+    ROW: "rows"
+}; /* corner, etc*/
+
 
 //FOR EACH BET TYPE
-function isStraitValid(bet) {
-    return boolean
+function isStraightValid(bet) {
+    return bet.type == betType.STRAIGHT && bet.value >= 0 && bet.value <= 36
 }
 
 //FOR EACH BET TYPE
-function isStraitWon(rouletteResult, bet) {
-    return boolean
+function isStraightWon(rouletteResult, bet) {
+    return rouletteResult.value == bet.value
 }
 
+//FOR EACH BET TYPE
+function isColorValid(bet) {
+    return bet.type == betType.COLOR && (bet.value == "red" || bet.value == "black")
+}
+
+//FOR EACH BET TYPE
+function isColorWon(rouletteResult, bet) {
+    return rouletteResult.colorUI == bet.value
+}
+
+function isEvenOddValid(bet) {
+    return bet.type == betType.EVENODD && (bet.value == "even" || bet.value == "odd")
+}
+
+//FOR EACH BET TYPE
+function isEvenOddWon(rouletteResult, bet) {
+    return rouletteResult.type == bet.value
+}
+
+function isDozenValid(bet) {
+    return bet.type == betType.DOZEN && ((bet.value >0 &&  bet.value <=12) || (bet.value >12 && bet.value <=24) || (bet.value >=25 && bet.value <=36))
+}
+
+function isDozenWon(rouletteResult, bet) {
+    return rouletteResult.value == bet.value
+
+
+
+function isColumnValid(bet) {
+    return bet.type == betType.COLUMN && (bet.value >= 1 && bet.value <= 3)
+
+function isColumnWon(rouletteResult, bet) {
+    return rouletteResult.value == bet.value ||
+           (rouletteResult.value % 3 == 1) || (rouletteResult.value % 3 == 2) || (rouletteResult.value % 3 == 3)
+}
+
+function isRowValid(bet) {
+    return bet.type == betType.ROW && ( (bet.value >= 1 && bet.value <= 3) || (bet.value >= 4 && bet.value <= 6) || (bet.value >= 7 && bet.value <= 9) ||
+    (bet.value >= 10 && bet.value <= 12) || (bet.value >= 13 && bet.value <= 15) || (bet.value >= 16 && bet.value <= 18) ||
+    (bet.value >= 19 && bet.value <= 21) || (bet.value >= 22 && bet.value <= 24) || (bet.value >= 25 && bet.value <= 27) ||
+    (bet.value >= 28 && bet.value <= 30) || (bet.value >= 31 && bet.value <= 33) || (bet.value >= 34 && bet.value <= 36) )
+}
+
+function isRowWon(rouletteResult, bet) {
+    return rouletteResult.value == bet.value
+}
 function makeBet(currentBalance) {
     //explain possible bets
-    alert()
+    alert("Possible bets are straight ( 0-36 winning 35:1), color (red/black winning 1:1)")
     //ask amount, bet type, fact bet value
+    var betType = prompt("input bet type")
+    var betValue = prompt("input bet value")
+    var betAmount = prompt("input bet Amount")
     var bet = {
         type: betType,
         value: betValue,
         amount: betAmount,
     }
     //validate made bet
-    isStraitValid()
-
-    return bet
+    switch (bet.type) {
+        case :
+            isStraightValid();
+        case :
+            isColorValid();
+        case :
+            isEvenOddValid();
+        case :
+            isDozenValid();
+        case :
+            isColumnValid();
+        case :
+            isRowValid();
+    default:
+        return bet;
 }
 
 function rollRoulette() {
     // rolling roulette
-
+    var index = Math.floor(Math.random()*37);
+    var resultRoulette = numbers[index];
     return resultRoulette
 }
 
-function checkBet(bet, rouletteResult) {
+function isBetWon(bet, rouletteResult) {
     //verify bet status
-    isStraitWon()
-
-    return boolean
+    isStraightWon()
+    isColorWon()
+    isEvenOddWon()
+    isDozenWon()
+    isColumnWon()
+    isRowWon()
+    return
 }
-
-
-
